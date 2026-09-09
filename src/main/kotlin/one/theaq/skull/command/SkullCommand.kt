a0b79c0bd1a83a4d4da7fd8cd5fb9d851606209d
Ivan Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-09-09 11:39:22 +0300

    prettier commands

--- src/main/kotlin/one/theaq/skull/command/SkullCommand.kt
+++ src/main/kotlin/one/theaq/skull/command/SkullCommand.kt
@@ -8,6 +8,7 @@ import net.minecraft.commands.Commands
 import net.minecraft.network.chat.Component
 import net.minecraft.server.permissions.Permission
 import net.minecraft.server.permissions.Permissions
+import net.minecraft.world.level.storage.ServerLevelData
 import one.theaq.skull.logic.SkullManager
 
 class SkullCommand: BaseCommand() {
@@ -30,22 +31,22 @@ class SkullCommand: BaseCommand() {
     }
 
     fun spawnSkull(context: CommandContext<CommandSourceStack>): Int {
-        context.source.sendSystemMessage(Component.literal("spawned skull at 0 0"))
         SkullManager.INSTANCE.createSkull(context.source.level)
+        context.source.sendSystemMessage(Component.literal("Spawned skull at 0 0"))
         return 0
     }
 
     fun deleteSkull(context: CommandContext<CommandSourceStack>): Int {
-        context.source.sendSystemMessage(Component.literal("spawned skull at 0 0"))
-        SkullManager.INSTANCE.getSkulls(context.source.level, context.source.position, DoubleArgumentType.getDouble(context, "radius")).forEach {
-            SkullManager.INSTANCE.removeSkull(it)
+        val removedSkulls = SkullManager.INSTANCE.getSkulls(context.source.level, context.source.position, DoubleArgumentType.getDouble(context, "radius")).count {
+            SkullManager.INSTANCE.removeSkull(it) >= 0
         }
+        context.source.sendSystemMessage(Component.literal("Destroyed $removedSkulls skulls"))
         return 0
     }
 
     fun deleteAllSkulls(context: CommandContext<CommandSourceStack>): Int {
-        context.source.sendSystemMessage(Component.literal("deleted all skulls"))
         SkullManager.INSTANCE.removeAllSkulls()
+        context.source.sendSystemMessage(Component.literal("Destroyed all skulls"))
         return 0
     }
 }
