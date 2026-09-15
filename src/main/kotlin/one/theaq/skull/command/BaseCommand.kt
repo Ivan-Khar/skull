a2944ade618b75debbbadc981f9145713829846f
Ivan-Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-09-15 18:31:02 +0300

    fixed command permissions

--- src/main/kotlin/one/theaq/skull/command/BaseCommand.kt
+++ src/main/kotlin/one/theaq/skull/command/BaseCommand.kt
@@ -19,7 +19,7 @@ abstract class BaseCommand {
         )
 
         val main = dispatcher.register(mainBuilder)
-        val alias = dispatcher.register(Commands.literal(getName()).redirect(main))
+        val alias = dispatcher.register(Commands.literal(getName()).requires(::permissionCheck).redirect(main))
     }
 
     open fun <T: ArgumentBuilder<CommandSourceStack, T>> commandBuilder(command: T): T {
