4f6ef7098cee00797819780813e88354dce29284
Ivan Khar <47220198+Ivan-Khar@users.noreply.github.com>
2026-09-15 15:16:22 +0300

    remove client classes

--- build.fabric-deobf.gradle.kts
+++ build.fabric-deobf.gradle.kts
@@ -121,10 +121,6 @@ loom {
 fletchingTable {
     fabric {
         entrypointMappings.put("fabric-datagen", "net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint")
-        entrypointMappings.put(
-            "fabric-client-gametest",
-            "net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest"
-        )
     }
 
     mixins.register("main") {
